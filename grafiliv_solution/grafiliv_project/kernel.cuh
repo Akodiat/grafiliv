243a10330eb137c162e6c535a942ddea7eca5368
Joakim Johansson <joajohan@student.chalmers.se>
2016-09-16 15:31:13 +0200

    Remove Visual Studio project from repo

--- grafiliv_solution/grafiliv_project/kernel.cuh
+++
@@ -1,6 +0,0 @@
-#ifndef KERNEL_CUH
-#define KERNEL_CUH
-
-void addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-#endif
--- grafiliv_solution/grafiliv_project/render.h
+++
@@ -1,7 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-void renderScene(void);
-void glInit(int argc, char **argv);
-
-#endif
