1895eb3e31a22bc028d6448a66ada0b3810d8a60
Joakim Johansson <joajohan@student.chalmers.se>
2016-09-26 15:34:25 +0200

    Add buoyancy and multiple organisms to celldiff

--- fluidix/cell_differentiation/celldiff.cu
+++ fluidix/cell_differentiation/celldiff.cu
@@ -11,6 +11,9 @@
 #define REPULSION_FORCE 150
 #define ATTRACTION_FORCE 40
 
+#define FLUID_DENSITY 1.0f
+#define G -9.81f
+
 using namespace std;
 
 __host__ __device__ bool operator==(xyz a, xyz b) {
@@ -22,14 +25,15 @@ __host__ __device__ bool operator==(xyz a, xyz b) {
 }
 
 struct Global {
-	int currInnovNumber;
 } g;
 
 struct Particle {
 	xyz r, v, f;
 	float energy;
 	float color;
+	float alpha;
 	float radius;
+	float density;
 	bool remove;
 	bool divide;
 	int linkSet;
@@ -40,6 +44,8 @@ struct Particle {
 FUNC_EACH(init,
 	p.r = make_xyz_uniform() * W;
 	p.energy = 1000.0f;
+	p.alpha  = 1.0f;
+	p.density = FLUID_DENSITY;
 	p.radius = 1.0f;
 	p.remove = false;
 	p.divide = false;
@@ -49,11 +55,19 @@ FUNC_EACH(init,
 
 // pair interaction
 FUNC_PAIR(particlePair,
-
 	float ratio = dr / range;
-	xyz f = u * (REPULSION_FORCE * (1-ratio) - ATTRACTION_FORCE * ratio );
+	float attraction = 0.0f;
+	if(p1.linkSet == p2.linkSet)
+		attraction = ATTRACTION_FORCE * ratio;
+	xyz f = u * (REPULSION_FORCE * (1-ratio) - attraction);
 	addVector(p1.f, f);
 	addVector(p2.f, -f);
+)
+
+// buoyancy 
+FUNC_EACH(buoyancy,
+	float volume = p.radius * p.radius * PI;
+	p.f.y += (p.density - FLUID_DENSITY) * G * volume;
 )
 
 FUNC_EACH(integrate,
@@ -71,7 +85,6 @@ FUNC_EACH(friction,
 )
 
 FUNC_EACH(checkDivide,
-	//p.color = p.energy / MAX_CELL_ENERGY;
 	if (p.energy > MAX_CELL_ENERGY && rnd_uniform() < 0.1f)
 		p.divide = true;
 	else if(p.energy <= 0)
@@ -90,24 +103,25 @@ FUNC_EACH(boundary,
 int main(int argc, char **argv) {
 	Fluidix<> *fx = new Fluidix<>(&g);
 
-	g.currInnovNumber = 0;
 	map<int, Genome> genomes;
 
-	int A = fx->createParticleSet(1);
+	int A = fx->createParticleSet(10);
 	fx->runEach(init(), A);
+	
 	Particle *pArray = fx->getParticleArray(A);
-
-	for (int step = 0; step < 1000; step++) {
+	for (int step = 0; step < 5000; step++) {
+		
 		Particle *p = fx->getParticleArray(A);
 		int n = fx->getParticleCount(A);
 		for (int i = 0; i < n; i++) {
 			if (p[i].remove == true)
 			{
 				//printf("Removing particle (%d)\n", i);
-				memcpy(&p[i], &p[n-1], sizeof(Particle)); 	// replace current with last
+/*				memcpy(&p[i], &p[n-1], sizeof(Particle)); 	// replace current with last
 				fx->applyParticleArray(A); 					// apply changes before any operation
 				fx->resizeParticleSet(A, --n); 				// delete the last particle and decrease n
-			}
+				Particle *p = fx->getParticleArray(A);
+*/			}
 			else if (p[i].divide == true)
 			{
 				//printf("Dividing particle (%d)\n", i);
@@ -117,7 +131,7 @@ int main(int argc, char **argv) {
 					p[i].linkSet = fx->createLinkSet();
 
 					int inputs = 4;
-					int outputs = 1;
+					int outputs = 3;
 
 					Genome g(inputs, outputs);
 
@@ -126,25 +140,36 @@ int main(int argc, char **argv) {
 					g.mutate();
 					g.mutate();
 
-					g.printGenome();
+					g.printMathematica();
 
 					genomes.insert({p[i].linkSet,	g});
+
+					vector<float> input(inputs, 0.0f);
+					vector<float> output = g.getOutput(input);
+	
+					p[i].color = output[0];
+					p[i].density = output[1];
+					p[i].remove = output[2]<=0;
 				}
-				
+
 				p[i].divide = false;
 				p[i].energy -= 0.1f;
 				p[i].energy /= 2;
 				fx->applyParticleArray(A); 
 				fx->resizeParticleSet(A, ++n); 				// add a new particle and increase n
+				Particle *p = fx->getParticleArray(A);
+
 				memcpy(&p[n-1], &p[i], sizeof(Particle)); 	// copy current to last
-				
+
 				// Displace particles from each other
 				xyz dr = xyz_norm(make_xyz_uniform()) * p[i].radius;		
 				p[i].r 	-= dr;
 				p[n-1].r 	+= dr;
 
 				//Create link between new particle and origin
-				fx->addLink(p[n-1].linkSet, A, p[n-1].origin, A, n-1);
+				//fx->addLink(p[n-1].linkSet, A, p[n-1].origin, A, n-1);
+				printf("parent link set: %i\tchild link set: %i\t",p[i].linkSet, p[n-1].linkSet);
+				fx->addLink(p[n-1].linkSet, A, i, A, n-1);
 
 				Genome genome = genomes.at(p[n-1].linkSet);
 
@@ -158,17 +183,21 @@ int main(int argc, char **argv) {
 
 				vector<float> output = genome.getOutput(input);
 
-				printf("input: ");  for(float i : input)  printf("%.2f ",i); printf("\n");
+				printf("input: ");  for(float i : input)  printf("%.2f ",i); printf("\t");
 				printf("output: "); for(float o : output) printf("%.2f ",o); printf("\n");
-				p[n-1].color = output[0];
 
+				p[n-1].color = output[0];
+				p[n-1].density = output[1];
+				p[n-1].remove = output[2]<=0;
+	
 				fx->applyParticleArray(A); 					// apply changes before any operation
 			}
 		}
 		//printf("number of particles: %d\n", n);
 
 		fx->runPair(particlePair(), A, A, RANGE);
-		fx->runEach(integrate(), A);	
+		fx->runEach(integrate(), A);
+		fx->runEach(buoyancy(), A);
 		//fx->runEach(photosynthesis(), A);	
 		fx->runEach(friction(), A);
 		fx->runEach(checkDivide(), A);
